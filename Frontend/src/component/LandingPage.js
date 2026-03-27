c43234e97cc8704e30b7d4121cd36cf848c7ddcf
Pavellooo <lpavelschak@gmail.com>
2026-03-27 00:27:51 -0400

    Remove redundant code in FlowInspectorPanel

--- Frontend/src/component/LandingPage.js
+++ Frontend/src/component/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import FlowInspectorPanel from './FlowInspectorPanel';
+
 
 
 // Landing page with navigation to login and tools
